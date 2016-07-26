0ad86adfe2645806c6324ade127dd0151cd68f78
Manuel Reinhard <manu@sprain.ch>
2016-07-26 16:44:23 +0200

    Completed RegisterAlias container

--- lib/SaferpayJson/Container/RegisterAlias.php
+++ lib/SaferpayJson/Container/RegisterAlias.php
@@ -15,6 +15,20 @@ class RegisterAlias
     protected $IdGenerator="RANDOM";
 
     /**
+     * @var string
+     * @SerializedName("Id")
+     * @Type("string")
+     */
+    protected $id;
+
+    /**
+     * @var int
+     * @SerializedName("Lifetime")
+     * @Type("integer")
+     */
+    protected $lifetime;
+
+    /**
      * @return string
      */
     public function getIdGenerator()
@@ -29,6 +43,44 @@ class RegisterAlias
     public function setIdGenerator($idGenerator)
     {
         $this->IdGenerator = $idGenerator;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return RegisterAlias
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLifetime()
+    {
+        return $this->lifetime;
+    }
+
+    /**
+     * @param int $lifetime
+     * @return RegisterAlias
+     */
+    public function setLifetime($lifetime)
+    {
+        $this->lifetime = $lifetime;
 
         return $this;
     }
