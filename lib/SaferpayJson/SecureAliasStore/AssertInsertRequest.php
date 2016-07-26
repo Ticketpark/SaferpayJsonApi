297dc79a93ebc97e23d9db704163c1e08ed5665d
Manuel Reinhard <manu@sprain.ch>
2016-07-26 16:46:50 +0200

    Cleaned up class structure of SecureAliasStore\ AssertInsertRequest

--- lib/SaferpayJson/SecureAliasStore/AssertInsertRequest.php
+++ lib/SaferpayJson/SecureAliasStore/AssertInsertRequest.php
@@ -2,11 +2,36 @@
 
 namespace Ticketpark\SaferpayJson\SecureAliasStore;
 
-use Ticketpark\SaferpayJson\PaymentPage\AssertRequest;
+use Ticketpark\SaferpayJson\Message\Request;
 
-class AssertInsertRequest extends AssertRequest
+class AssertInsertRequest extends Request
 {
     const API_PATH = '/Payment/v1/Alias/AssertInsert';
 
     const RESPONSE_CLASS = 'Ticketpark\SaferpayJson\SecureAliasStore\AssertInsertResponse';
+
+    /**
+     * @var string
+     * @SerializedName("Token")
+     */
+    protected $token;
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     * @return AssertInsertRequest
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
 }
