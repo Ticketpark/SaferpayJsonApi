7a74bc748498c8c87396d7d0be0edfc1e8124522
Steven <steven@fatpanda.io>
2019-05-10 11:35:36 +0200

    Added example for cancel requests

---
+++ example/Transaction/2-example-cancel.php
@@ -0,0 +1,37 @@
+<?php
+
+use \Ticketpark\SaferpayJson\Container;
+use \Ticketpark\SaferpayJson\Message\ErrorResponse;
+use \Ticketpark\SaferpayJson\Transaction\CancelRequest;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../credentials.php';
+
+// A transactionid you received with a successful assert request (see ../PaymentPage/2-example-assert.php)
+
+$transactionId = 'xxx';
+
+// Step 1:
+// Prepare the cancel request
+// https://saferpay.github.io/jsonapi/#Payment_v1_Transaction_Cancel
+
+$requestHeader = (new Container\RequestHeader())
+    ->setCustomerId($customerId)
+    ->setRequestId(uniqid());
+
+$transactionReference = (new Container\TransactionReference())
+    ->setTransactionId($transactionId);
+
+$response = (new CancelRequest($apiKey, $apiSecret))
+    ->setRequestHeader($requestHeader)
+    ->setTransactionReference($transactionReference)
+    ->execute();
+
+// Step 2:
+// Check for successful response
+
+if ($response instanceof ErrorResponse) {
+    die($response->getErrorMessage());
+}
+
+echo 'The transaction has successfully been canceled! Transaction-ID: ' . $response->getTransactionId();
