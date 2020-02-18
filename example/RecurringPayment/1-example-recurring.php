38a49c5759778913b2bc816ae04ebeb14f5ad51d
Emirald Mateli <aldo.mateli@gmail.com>
2020-02-18 10:57:34 +0100

    Add recurring payments support

---
+++ example/RecurringPayment/1-example-recurring.php
@@ -0,0 +1,42 @@
+<?php
+
+use Ticketpark\SaferpayJson\Container;
+use Ticketpark\SaferpayJson\Message\ErrorResponse;
+use Ticketpark\SaferpayJson\PaymentPage\AuthorizeReferencedRequest;
+use Ticketpark\SaferpayJson\PaymentPage\AuthorizeReferencedResponse;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../credentials.php';
+
+// ID of an existing captured transaction flagged as recurring. eg: $payment->setRecurring(new Recurring(true))
+$transactionId = '';
+
+$requestHeader = (new Container\RequestHeader())
+    ->setCustomerId($customerId)
+    ->setRequestId(uniqid());
+
+$amount = (new Container\Amount())
+    ->setCurrencyCode('CHF')
+    ->setValue(5000); // amount in cents
+
+$response = (new AuthorizeReferencedRequest($apiKey, $apiSecret, true))
+    ->setRequestHeader($requestHeader)
+    ->setPayment((new Container\Payment())->setAmount($amount))
+    ->setTransactionReference((new Container\TransactionReference())->setTransactionId($transactionId))
+    ->setTerminalId($terminalId)
+    ->execute();
+
+if ($response instanceof ErrorResponse) {
+    die($response->getErrorMessage());
+}
+
+/** @var AuthorizeReferencedResponse $response */
+echo sprintf("Transaction ID: %s, Status: %s, via %s (%s)",
+    $response->getTransaction()->getId(),
+    $response->getTransaction()->getStatus(),
+    $response->getPaymentMeans()->getBrand()->getName(),
+    $response->getPaymentMeans()->getDisplayText()
+);
+
+// Recurring payment transactions still need to be captured.
+// see: Transaction/1-example-catpure.php
---
+++ lib/SaferpayJson/PaymentPage/AuthorizeReferencedResponse.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Ticketpark\SaferpayJson\PaymentPage;
+
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+use Ticketpark\SaferpayJson\Container\PaymentMeans;
+use Ticketpark\SaferpayJson\Container\Transaction;
+use Ticketpark\SaferpayJson\Message\Response;
+
+class AuthorizeReferencedResponse extends Response
+{
+    /**
+     * @var Transaction
+     * @SerializedName("Transaction")
+     * @Type("Ticketpark\SaferpayJson\Container\Transaction")
+     */
+    protected $transaction;
+
+    /**
+     * @var PaymentMeans
+     * @SerializedName("PaymentMeans")
+     * @Type("Ticketpark\SaferpayJson\Container\PaymentMeans")
+     */
+    protected $paymentMeans;
+
+    /**
+     * @return Transaction
+     */
+    public function getTransaction()
+    {
+        return $this->transaction;
+    }
+
+    /**
+     * @param Transaction $transaction
+     */
+    public function setTransaction($transaction)
+    {
+        $this->transaction = $transaction;
+    }
+
+    /**
+     * @return PaymentMeans
+     */
+    public function getPaymentMeans()
+    {
+        return $this->paymentMeans;
+    }
+
+    /**
+     * @param PaymentMeans $paymentMeans
+     */
+    public function setPaymentMeans($paymentMeans)
+    {
+        $this->paymentMeans = $paymentMeans;
+    }
+}
