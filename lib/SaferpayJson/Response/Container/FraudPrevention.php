acd213cb749cfe8b1a2d34227bbf0fbdfa01f50a
jdraguljic <josip.draguljic@world4you.com>
2023-10-09 15:07:34 +0200

    Add missing type annotation to result property in FraudPrevention.php

--- lib/SaferpayJson/Response/Container/FraudPrevention.php
+++ lib/SaferpayJson/Response/Container/FraudPrevention.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Ticketpark\SaferpayJson\Response\Container;
 
 use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
 
 final class FraudPrevention
 {
@@ -14,6 +15,7 @@ final class FraudPrevention
     /**
      * @var string|null
      * @SerializedName("Result")
+     * @Type("string")
      */
     private $result;
 
