8a949be8a0d7c23af6687501a141ba5e7d5ea563
Manuel Reinhard <manu@sprain.ch>
2024-06-27 15:57:38 +0200

    Fix whitespace

--- example/Transaction/example-inquire.php
+++ example/Transaction/example-inquire.php
@@ -7,6 +7,7 @@ use Ticketpark\SaferpayJson\Request\Transaction\InquireRequest;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../credentials.php';
+
 // A transaction id you received with a successful assert request (see ../PaymentPage/example-assert.php)
 
 $transactionId = 'xxx';
